b1360a5d2fbce0b7c3ec9cf45d08d62f86b7d131
Justin Welch <justin.welch075@gmail.com>
2021-05-24 09:51:24 -0500

    trying to fix a problem with heroku

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,80 +1,70 @@
-const { User, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
-
+console.log(User);
 const resolvers = {
-
     Query: {
-
-        //get a user by username
         me: async (parent, args, context) => {
-
-            if(context.user) {
-                const userData = await User.findOne({})
+            if (context.user) {
+              const userData = await User.findOne({ _id: context.user._id })
                 .select('-__v -password')
-                .populate('books')
-            
-                return userData;
+                .populate('savedbooks');
+      
+              return userData;
             }
-
-            throw new AuthenticationError('Not logged in')
-
-        },
-
+      
+            throw new AuthenticationError('Not logged in');
+        }
     },
 
     Mutation: {
-
         addUser: async (parent, args) => {
             const user = await User.create(args);
             const token = signToken(user);
-          
-            return {token, user};
+      
+            return { token, user };
         },
 
-        login: async (parent, {email, password}) => {
-            const user = await User.findOne({email});
-
-            if(!user) {
-                throw new AuthenticationError('Incorrect credentials');
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
             }
-
+      
             const correctPw = await user.isCorrectPassword(password);
-
-            if(!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
             }
-
+      
             const token = signToken(user);
-            return {token, user};
-    
+            return { token, user };
         },
 
-        saveBook: async (parent, args, context) => {
+        saveBook: async (parent, { book }, context) => {
             if (context.user) {
-             const updatedUser =  await User.findByIdAndUpdate(
-                { _id: context.user._id },
-                { $addToSet: { savedBooks: args.input } },
-                { new: true }
-              );
-          
-            return updatedUser;
+                const addBook = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: book } },
+                    { new: true }
+                );
+        
+                return addBook;
             }
-          
+
             throw new AuthenticationError('You need to be logged in!');
         },
 
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const removeBook = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                );
 
-
-        removeBook: async (parent, args, context) => {
-            if(context.user) {
-            const updatedUser = await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $pull: { savedBooks: { bookId: args.bookId } } },
-                { new: true }
-            );
-
-            return updatedUser;
+                return removeBook;
             }
 
             throw new AuthenticationError('You need to be logged in!');

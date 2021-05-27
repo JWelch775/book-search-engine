a66287209d96f8f20480a19558c7414483a87c7e
Justin Welch <justin.welch075@gmail.com>
2021-05-27 11:23:22 -0500

    update

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const db = require('./config/connection');
 const {authMiddleware} = require('./utils/auth');
 
 const path = require('path');
-//const routes = require('./routes');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();

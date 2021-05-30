a7723eb4852ac340ff354edf00c349c3d4c95c6e
Justin Welch <justin.welch075@gmail.com>
2021-05-30 12:00:22 -0500

    update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
-
+const dotenv = require("dotenv")
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+dotenv.config()
 
 const { typeDefs, resolvers } = require('./schemas');
 

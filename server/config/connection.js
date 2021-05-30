90dd1349e60c85defb60241e20ca39a6e4c21697
Justin Welch <justin.welch075@gmail.com>
2021-05-30 14:37:36 -0500

    update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
